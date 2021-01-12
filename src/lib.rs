9a5445b1b2dfbcf22298dbae466cf7699557a1b9
Daniel Linares Gil <daniel.linares@ciencias.unam.mx>
2021-01-12 01:11:21 -0600

    Some design ideas

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
 mod shamir;
+
+use std::error::Error;
+
+pub struct EncryptConfig {
+    pub total_evals: u32,
+    pub min_required_evals: u32,
+    pub shares_filepath: String,
+    pub file_to_encrypt: String,
+    pub password: String,
+}
+
+pub struct DecryptConfig {
+    pub encrypted_file: String,
+    pub keys_file: String,
+}
+
+pub enum Config {
+    Encrypt(EncryptConfig),
+    Decrypt(DecryptConfig),
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    Ok(()) // TODO
+}
--- src/shamir/math.rs
+++ src/shamir/math.rs
@@ -1,5 +1,8 @@
 use std::ops::{Add, Div, Mul, Sub};
 
+pub use rug::integer::ParseIntegerError;
+use rug::Integer;
+
 /// Represents a mathemathical field.
 /// Provides methods for adding, multiplying
 /// and getting identities and inverses.
@@ -43,3 +46,49 @@ pub trait Field: Sized + Add + Sub + Mul + Div + PartialEq {
         *self == Self::one()
     }
 }
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                 Modular Integer Structure                      //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+
+/// Simple struct for representing prime numbers to use with modular
+/// integers.
+///
+/// The objective of this type is to provide a unique instance from which
+/// references can be borrowed when creating and manipulating modular integers.
+pub struct Prime {
+    value: Integer,
+}
+
+impl Prime {
+    /// Parses the number represented by the string and returns it
+    /// as a Prime.
+    ///
+    ///
+    /// This methods returns an error if an error occurs while parsing.
+    ///
+    /// # Notes
+    ///
+    /// This methods only wraps the given string as a Prime number,
+    /// this method does not check if the integer represented by
+    /// this string is actually a prime.
+    fn parse(string: &str) -> Result<Prime, ParseIntegerError> {
+        Ok(Prime {
+            value: Integer::from(Integer::parse(string)?),
+        })
+    }
+}
+
+/// This structure represents a modular integer number.
+/// This type implements the Field trait in order to provide
+/// field behaviour for modular integers, but it is required
+/// that users of this struct use actually prime numbers when
+/// instantiating this type.
+pub struct ModInteger<'a> {
+    value: Integer,
+    prime: &'a Prime,
+}
+
+impl ModInteger {}
