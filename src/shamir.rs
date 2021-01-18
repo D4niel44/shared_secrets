4d09db3cf55b77f659aadeee5d8799d79b975ab7
Daniel Linares Gil <daniel.linares@ciencias.unam.mx>
2021-01-18 16:43:55 -0600

    Better error handling shamir

--- src/shamir.rs
+++ src/shamir.rs
@@ -1,7 +1,8 @@
 use std::collections::{hash_set, HashSet};
+use std::error::Error;
 
-use crate::math::random::Rng;
-use crate::math::{ModInteger, Polynomial, Prime};
+use crate::math::{error::ParseIntegerError, random::Rng};
+use crate::math::{Evaluation, ModInteger, Polynomial, Prime};
 
 const PRIME_257: &str =
     "208351617316091241234326746312124448251235562226470491514186331217050270460481";
@@ -9,6 +10,7 @@ const PRIME_257: &str =
 /// A share of the secret.
 pub type Share = (String, String);
 
+/// A owning Iterator over shares.
 pub type ShareIter = hash_set::IntoIter<Share>;
 
 /// Splits a secret using shamir secret sharing scheme.
@@ -20,7 +22,7 @@ pub type ShareIter = hash_set::IntoIter<Share>;
 /// - k: The minimum number of shares to decipher the secret (0 < k <= n).
 ///
 /// # Returns
-/// A vector with n evaluations of teh polynomial.
+/// A vector with n evaluations of the polynomial.
 ///
 /// # Panics
 ///
@@ -63,20 +65,34 @@ pub fn split_secret(secret: &[u8], n: usize, k: usize) -> ShareIter {
     evaluations.into_iter()
 }
 
-pub fn recover_secret(evals: impl Iterator<Item = Share>) -> Vec<u8> {
+/// Recovers a secret given an Iterator of Shares.
+///
+/// # Parameters
+///
+/// - evals: An Iterator of shares, require that each share has a
+/// unique first element.
+///
+/// # Returns
+///
+/// A vector of bytest containing the original secret.
+///
+/// # Errors
+///
+/// This method returns an error if it cannot parse the integers
+/// or if there are two shares with the same first element.
+pub fn recover_secret(shares: impl Iterator<Item = Share>) -> Result<Vec<u8>, Box<dyn Error>> {
     let prime = Prime::parse(PRIME_257).unwrap();
-    let polynomial = Polynomial::from_evals(
-        evals
-            .map(|(x, y)| {
-                (
-                    ModInteger::parse(&x, &prime).unwrap(),
-                    ModInteger::parse(&y, &prime).unwrap(),
-                )
-            })
-            .collect(),
-    );
+    let evaluations = shares
+        .map::<Result<Evaluation, ParseIntegerError>, _>(|(x, y)| {
+            Ok((
+                ModInteger::parse(&x, &prime)?,
+                ModInteger::parse(&y, &prime)?,
+            ))
+        })
+        .collect::<Result<Vec<Evaluation>, _>>()?;
+    let polynomial = Polynomial::from_evals(evaluations);
     let (_, secret_number) = polynomial.eval(ModInteger::parse("0", &prime).unwrap());
-    secret_number.to_digits()
+    Ok(secret_number.to_digits())
 }
 
 #[cfg(test)]
@@ -85,43 +101,47 @@ mod tests {
     use super::*;
 
     macro_rules! split_secret_test {
-        ($secret:expr, $n:expr, $k:expr) => {
+        ($secret:expr, $n:expr, $k:expr, $take:expr) => {
             let secret = $secret;
             let result = split_secret(&secret, $n, $k);
             assert_eq!(result.len(), $n);
-            let returned_secret = recover_secret(result.take($k));
+            let returned_secret = recover_secret(result.take($take));
             assert_eq!(secret, returned_secret);
         };
     }
 
     #[test]
     fn secret_empty_vec() {
-        split_secret_test!(vec![], 5, 3);
+        split_secret_test!(vec![], 5, 3, 3);
     }
 
     #[test]
     fn secret_zeroes_vec() {
-        split_secret_test!(vec![0x0u8, 0x0u8, 0x0u8], 4, 3);
+        split_secret_test!(vec![], 4, 3, 3);
     }
 
     #[test]
     fn secret_minimum_n() {
-        split_secret_test!(vec![0x12u8, 0x87u8], 2, 1);
+        split_secret_test!(vec![0x12u8, 0x87u8], 3, 1, 1);
     }
 
     #[test]
     fn secret_minimum_k() {
-        split_secret_test!(vec![0x67, 0xa, 0xc, 0xe], 10, 1);
+        split_secret_test!(vec![0x67, 0xa, 0xc, 0xe], 10, 1, 1);
     }
 
     #[test]
     fn secret_n_equals_k() {
-        split_secret_test!(vec![0x1u8, 0x2u8, 0x3u8, 0x35u8], 5, 5);
+        split_secret_test!(vec![0x1u8, 0x2u8, 0x3u8, 0x35u8], 5, 5, 5);
     }
 
-    // split a secret and recover it.
     #[test]
     fn secret_integrity() {
-        split_secret_test!(vec![0x11u8, 0xffu8, 0x35u8, 0x4eu8], 4, 2);
+        split_secret_test!(vec![0x11u8, 0xffu8, 0x35u8, 0x4eu8], 4, 2, 2);
+    }
+
+    #[test]
+    fn secret_more_than_k_evals() {
+        split_secret_test!(vec![0xafu8, 0xbbu8, 0x13u8, 0x01u8], 10, 3, 9);
     }
 }
