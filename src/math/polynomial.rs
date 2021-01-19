c828852a785a564c5c48152c23417e2416761a5f
Daniel Linares Gil <daniel.linares@ciencias.unam.mx>
2021-01-18 19:07:46 -0600

    Added error handling and Documentation

--- src/math/polynomial.rs
+++ src/math/polynomial.rs
@@ -1,31 +1,59 @@
+use std::collections::HashMap;
+
 use crate::math::*;
+use crate::math::error::ValueError;
 
 pub type Evaluation<'a> = (ModInteger<'a>, ModInteger<'a>);
 
+/// A Modular Integers coefficients polynomial
 pub struct CoeffPolynomial<'a> {
     coefficients: Vec<ModInteger<'a>>,
 }
 
+/// A Modular Integer interpolation polynomial
 pub struct InterpolationPolynomial<'a> {
     evaluations: Vec<Evaluation<'a>>,
 }
 
+/// A polynomial, either created from coefficients
+/// or from interpolation.
 pub enum Polynomial<'a> {
     Coefficients(CoeffPolynomial<'a>),
     Interpolation(InterpolationPolynomial<'a>),
 }
 
 impl<'a> Polynomial<'a> {
-    pub fn from_coef(coeffs: Vec<ModInteger<'a>>) -> Self {
-        Polynomial::Coefficients(CoeffPolynomial {
-            coefficients: coeffs,
-        })
+    /// Creates a new polynomial given the vector of coefficients.
+    ///
+    /// The x[i] element of the vector is treated as the coefficient
+    /// for the term x^i
+    ///
+    /// # Parameters
+    ///
+    /// - coeffs: A vector of coefficients. It is required that this
+    /// vector is not empty and that the last coefficient is not zero.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if the constraints above are not met.
+    pub fn from_coeffs(coeffs: Vec<ModInteger<'a>>) -> Self {
+        Polynomial::Coefficients(CoeffPolynomial::new(coeffs))
     }
 
-    pub fn from_evals(evals: Vec<Evaluation<'a>>) -> Self {
-        Polynomial::Interpolation(InterpolationPolynomial { evaluations: evals })
+    /// Creates a new polynomial given a vector of evaluations.
+    ///
+    /// # Parameters
+    ///
+    /// - evaluations: A vector of Evaluations. Each evaluation should have a unique
+    /// first value and the evaluations vector should not be empty.
+    ///
+    /// # Returns
+    /// A polynomial or an error if the constrains are not met.
+    pub fn from_evals(evaluations: Vec<Evaluation<'a>>) -> Result<Self, ValueError> {
+        Ok(Polynomial::Interpolation(InterpolationPolynomial::new(evaluations)?))
     }
 
+    /// Returns the result from evaluating this polynomial.
     pub fn eval(&self, x: ModInteger<'a>) -> Evaluation<'a> {
         match self {
             Polynomial::Coefficients(poly) => poly.eval(x),
@@ -35,6 +63,26 @@ impl<'a> Polynomial<'a> {
 }
 
 impl<'a> CoeffPolynomial<'a> {
+
+    /// Same as Polynomial::from_coeffs, but returns a CoeffPolynomial instead.
+    ///
+    /// It is encouraged to create this type of polynomial using Polynomial::from_coeffs
+    /// instead of using this method.
+    pub fn new(coefficients: Vec<ModInteger<'a>>) -> Self {
+        if coefficients.len() == 0 {
+            panic!("Tried to create polynomial with zero elements");
+        }
+        if *coefficients.last().unwrap() == coefficients[0].zero() {
+            panic!("Last coefficient is zero");
+        }
+        CoeffPolynomial {
+            coefficients,
+        }
+    }
+
+    /// Returns the result from evaluating this polynomial.
+    ///
+    /// Discouraged in favor of Polynomial.eval()
     pub fn eval(&self, x: ModInteger<'a>) -> Evaluation<'a> {
         let y = self
             .coefficients
@@ -46,6 +94,30 @@ impl<'a> CoeffPolynomial<'a> {
 }
 
 impl<'a> InterpolationPolynomial<'a> {
+
+    /// Same as Polynomial::from_evals, but returns a InterpolationPolynomial instead.
+    ///
+    /// It is encouraged to create this type of polynomial using Polynomial::from_evals
+    /// instead of using this method.
+    pub fn new(evaluations: Vec<Evaluation<'a>>) -> Result<Self, ValueError> {
+        if evaluations.len() == 0 {
+            panic!("no evaluations were provided");
+        }
+        let mut unique_evals = HashMap::with_capacity(evaluations.len());
+        for (x, y) in &evaluations {
+            if unique_evals.contains_key(x) {
+                return Err(ValueError("Duplicated x across Evaluation".into()));
+            }
+            unique_evals.insert(x, y);
+        }
+        Ok(InterpolationPolynomial {
+            evaluations
+        })
+    }
+
+    /// Returns the result from evaluating this polynomial.
+    ///
+    /// Discouraged in favor of Polynomial.eval()
     pub fn eval(&self, x: ModInteger<'a>) -> Evaluation<'a> {
         let mut result = self.evaluations[0].0.zero();
         for (i, (_, y)) in self.evaluations.iter().enumerate() {
@@ -54,6 +126,7 @@ impl<'a> InterpolationPolynomial<'a> {
         (x, result)
     }
 
+    // evaluates the ith Lagrange base polynomial
     fn eval_base_polynomial(&self, x: &ModInteger<'a>, i: usize) -> ModInteger<'a> {
         let (x_i, _) = &self.evaluations[i];
         let mut result = x_i.one();
@@ -80,7 +153,7 @@ mod tests {
         let prime = Prime::parse("648863").unwrap();
         let x = ModInteger::parse("3", &prime).unwrap();
         let one = x.one();
-        let polynomial = Polynomial::from_coef(vec![x.one(), x.one(), x]);
+        let polynomial = Polynomial::from_coeffs(vec![x.one(), x.one(), x]);
         let (_, result) = polynomial.eval(one);
 
         assert_eq!(result, ModInteger::parse("5", &prime).unwrap());
@@ -96,7 +169,7 @@ mod tests {
         let y_2 = ModInteger::parse("0", &prime).unwrap();
         let y_3 = ModInteger::parse("6", &prime).unwrap();
         let zero = x_1.zero();
-        let poly = Polynomial::from_evals(vec![(x_1, y_1), (x_2, y_2), (x_3, y_3)]);
+        let poly = Polynomial::from_evals(vec![(x_1, y_1), (x_2, y_2), (x_3, y_3)]).unwrap();
         let (x, y) = poly.eval(zero);
 
         assert_eq!(x, ModInteger::zero(&prime));
