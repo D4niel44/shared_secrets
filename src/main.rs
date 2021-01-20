04f6647e0416a9c06e507a5a0ac1ac9cc28bb234
Daniel Linares Gil <daniel.linares@ciencias.unam.mx>
2021-01-20 01:27:18 -0600

    main and Args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process;
+
+use clap;
+
+use shared_secrets::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = clap::load_yaml!("../config/args.yaml");
+    let app = clap::App::from_yaml(yaml);
+
+    let config = Config::new(app.get_matches()).unwrap_or_else(|e| {
+        eprintln!("Problem parsing argument: {}", e);
+        process::exit(1);
+    });
+
+    if let Err(e) = shared_secrets::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
